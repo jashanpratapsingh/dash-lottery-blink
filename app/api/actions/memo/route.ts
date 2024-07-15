749fc656894a9bd6bad54e8fcbcd3daeadc90361
jashanpratapsingh <jashanpratap123@gmail.com>
2024-07-15 17:56:24 +0400

    Added multiple inputs into the blink

--- app/api/actions/memo/route.ts
+++ app/api/actions/memo/route.ts
@@ -5,10 +5,32 @@ export const GET = (req: Request) => {
 
     const payload : ActionGetResponse = {
         icon: new URL("/dash_lottery.png", new URL(req.url).origin).toString(),
-        label: "Send Memo",
-        description: "This is a super simple action",
-        title: "Memo Demo"
+        label: "Enter your 3 lottery numbers",
+        title: "DASH Lottery",
+        description: "Every Friday buy a ticket to win the Jackpot Select for 3 numbers for the Lottery. Enter the numbers between 0 and 20. For example, you could select: 2,14,7",
+        links: {
+            actions: [
+              {
+                href: `/api/actions/memo/numbers`,
+                label: 'Donate',
+                parameters: [
+                  {
+                    name: "first_number",
+                    label: 'Enter the first number',
+                  },
+                  {
+                    name: "second_numbers",
+                    label: 'Enter the second number',
+                  },
+                  {
+                    name: "thrid_number",
+                    label: "Enter the thrid number"
+                  }
+                ],
+              },
+            ]
     }
+}
 
     return Response.json(payload, {headers: ACTIONS_CORS_HEADERS});  
 }
